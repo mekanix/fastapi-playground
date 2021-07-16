fa556c48cde4f20a6ed87b166f0af46e0e91b609
Goran Mekić <meka@tilda.center>
2021-07-16 03:40:07 +0200

    Initial commit

---
+++ bin/init.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+export OFFLINE=${OFFLINE:="no"}
+BIN_DIR=`dirname $0`
+. ${BIN_DIR}/common.sh
+
+
+if [ "${OFFLINE}" = "yes" ]; then
+  setup no
+else
+  setup
+fi
+
+# if [ -e "alembic/versions" ]; then
+  # alembic upgrade head
+# fi
